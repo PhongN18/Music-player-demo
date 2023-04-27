f9cbbb61557edd204659572125f13b2242ebf2e1
PhongN18 <chelsea2009021808@gmail.com>
2023-04-27 08:00:11 +0700

    next, prev + progress bar + seek + random

--- main.js
+++ main.js
@@ -2,12 +2,21 @@ const $ = document.querySelector.bind(document)
 const $$ = document.querySelectorAll.bind(document)
 
 const heading = $('header h2')
+const cd = $('.cd')
 const cdThumb = $('.cd-thumb')
 const audio = $('#audio')
-const cd = $('.cd')
+const player = $('.player')
+const progress = $('#progress')
+const nextBtn = $('.btn-next')
+const prevBtn = $('.btn-prev')
+const randomBtn = $('.btn-random')
+
+const playBtn = $('.btn-toggle-play')
 
 const app = {
     currentIndex: 0,
+    isPlaying: false,
+    isRandom: false,
     // Songs list
     songs: [
         {
@@ -17,52 +26,58 @@ const app = {
             image: './assets/img/song1.jpg'
         },
         {
-            name: 'Whisper',
-            singer: 'Hoaprox',
+            name: 'Whisper (Hoaprox Official Remix)',
+            singer: 'Boombox Cartel ft Nevve',
             path: './assets/audio/song2.mp3',
             image: './assets/img/song2.jpg'
         },
         {
-            name: 'I can\'t find you',
+            name: 'I CAN\'T FIND YOU',
             singer: 'Hoaprox',
             path: './assets/audio/song3.mp3',
             image: './assets/img/song3.jpg'
         },
         {
-            name: 'Nevada',
-            singer: 'Vicetone',
-            path: './assets/audio/song1.mp3',
-            image: './assets/img/song1.jpg'
+            name: 'Safe And Sound',
+            singer: 'Different Heaven',
+            path: './assets/audio/song4.mp3',
+            image: './assets/img/song4.jpg'
         },
         {
-            name: 'Whisper',
-            singer: 'Hoaprox',
-            path: './assets/audio/song2.mp3',
-            image: './assets/img/song2.jpg'
+            name: 'Time',
+            singer: 'MKJ',
+            path: './assets/audio/song5.mp3',
+            image: './assets/img/song5.jpg'
         },
         {
-            name: 'I can\'t find you',
-            singer: 'Hoaprox',
-            path: './assets/audio/song3.mp3',
-            image: './assets/img/song3.jpg'
+            name: 'Light it up',
+            singer: 'Robin Hustin x TobiMorrow ft Jex',
+            path: './assets/audio/song6.mp3',
+            image: './assets/img/song6.jpg'
         },
         {
-            name: 'Nevada',
-            singer: 'Vicetone',
-            path: './assets/audio/song1.mp3',
-            image: './assets/img/song1.jpg'
+            name: 'Highscore',
+            singer: 'Teminite & Panda Eyes',
+            path: './assets/audio/song7.mp3',
+            image: './assets/img/song7.jpg'
         },
         {
-            name: 'Whisper',
-            singer: 'Hoaprox',
-            path: './assets/audio/song2.mp3',
-            image: './assets/img/song2.jpg'
+            name: 'Fade Away (DEAF KEV Remix)',
+            singer: 'Jacob Tillberg',
+            path: './assets/audio/song8.mp3',
+            image: './assets/img/song8.jpg'
         },
         {
-            name: 'I can\'t find you',
-            singer: 'Hoaprox',
-            path: './assets/audio/song3.mp3',
-            image: './assets/img/song3.jpg'
+            name: 'Why Do I',
+            singer: 'Unknown Brain ft Bri Tolani',
+            path: './assets/audio/song9.mp3',
+            image: './assets/img/song9.jpg'
+        },
+        {
+            name: 'Lullaby',
+            singer: 'R3HAB x Mike Williams',
+            path: './assets/audio/song10.mp3',
+            image: './assets/img/song10.jpg'
         },
     ],
     // Render playlist
@@ -96,7 +111,21 @@ const app = {
     },
 
     handleEvents: function () {
+        // constant representing app
+        const _this = this
         const cdWidth = cd.offsetWidth
+
+        // CD thumbnail rotate
+        const cdThumbAnimate = cdThumb.animate([
+            { transform: 'rotate(360deg)' }
+        ], {
+            duration: 30000, // 10s
+            iterations: Infinity // loop ifinite times
+        })
+
+        cdThumbAnimate.pause()
+
+        // CD thumbnail width handle
         document.onscroll = function() {
             const scrollTop = document.documentElement.scrollTop || window.scrollY     
             const newCDWidth = cdWidth - scrollTop
@@ -104,12 +133,102 @@ const app = {
             cd.style.width = newCDWidth > 0 ? newCDWidth + 'px' : 0
             cd.style.opacity = newCDWidth / cdWidth
         }
+
+        // Press play handle
+        playBtn.onclick = function() {
+            if (!_this.isPlaying) {
+                audio.play()
+            } else {
+                audio.pause()
+            }
+        }
+
+        // When song is played
+        audio.onplay = function() {
+            _this.isPlaying = true
+            player.classList.add('playing')
+            cdThumbAnimate.play()
+        }
+
+        // When song is paused
+        audio.onpause = function() {
+            _this.isPlaying = false
+            player.classList.remove('playing')
+            cdThumbAnimate.pause()
+        }
+
+        // Song progress bar
+        audio.ontimeupdate = function() {
+            if(audio.duration) {
+                const progressPercent = audio.currentTime / audio.duration * 100
+                progress.value = progressPercent 
+            }
+        }
+
+        // Seek handle
+        progress.onchange = function(e) {
+            const seekTime = audio.duration / 100 * e.target.value 
+            audio.currentTime = seekTime
+        }
+
+        // When press next song
+        nextBtn.onclick = function() {
+            if(_this.isRandom) {
+                _this.playRandom()
+            } else {
+                _this.nextSong()
+            }
+            audio.play()
+        }
+
+        // When press prev song
+        prevBtn.onclick = function() {
+            if (_this.isRandom) {
+                _this.playRandom()
+            } else {
+                _this.prevSong()
+            }
+            audio.play()
+        }
+
+        // Random btn
+        randomBtn.onclick = function() {
+            _this.isRandom = !_this.isRandom
+            randomBtn.classList.toggle('active', _this.isRandom)
+            // toggle 2nd param -> bool, if true => add, else remove
+        }
     },
 
     loadCurrentSong: function () {
         heading.textContent = this.currentSong.name
         cdThumb.style.backgroundImage = `url('${this.currentSong.image}')`
         audio.src = this.currentSong.path
+    },
+
+    nextSong: function() {
+        this.currentIndex++
+        if(this.currentIndex >= this.songs.length) {
+            this.currentIndex = 0
+        }
+        this.loadCurrentSong()
+    },
+
+    prevSong: function() {
+        this.currentIndex--
+        if(this.currentIndex < 0) {
+            this.currentIndex = this.songs.length - 1
+        }
+        this.loadCurrentSong()
+    },
+
+    playRandom: function() {
+        let randomIndex
+        do {
+            randomIndex = Math.floor(Math.random() * this.songs.length)
+        } while (this.currentIndex === randomIndex)
+        
+        this.currentIndex = randomIndex
+        this.loadCurrentSong()
     },
 
     start: function () {
